c39ad8087d918af96b4ecd8e7c21890138c8d680
TarunQT <singhtarunpreet823@gmail.com>
2026-08-27 15:25:17 +0530

    Fix TS Touch type error

--- src/components/tactical/tactical-canvas.tsx
+++ src/components/tactical/tactical-canvas.tsx
@@ -273,10 +273,10 @@ export function TacticalCanvas({
   };
 
   // --- Touch handlers ---
-  const getTouchDist = (t1: Touch, t2: Touch) =>
+  const getTouchDist = (t1: { clientX: number; clientY: number }, t2: { clientX: number; clientY: number }) =>
     Math.hypot(t2.clientX - t1.clientX, t2.clientY - t1.clientY);
 
-  const getTouchCenter = (t1: Touch, t2: Touch) => ({
+  const getTouchCenter = (t1: { clientX: number; clientY: number }, t2: { clientX: number; clientY: number }) => ({
     x: (t1.clientX + t2.clientX) / 2,
     y: (t1.clientY + t2.clientY) / 2,
   });
